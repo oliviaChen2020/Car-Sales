1d348ad3dc10669aa59332dcb41e464511199a70
Olivia <olivia@macbook-pro.myfiosgateway.com>
2020-11-03 22:40:37 -0500

    Project Redo

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 
-const Total = props => {
+const Total = (props) => {
   return (
     <div className="content">
       <h4>Total Amount: ${props.car.price + props.additionalPrice}</h4>
     </div>
   );
 };
-const mapStateToProps = state =>{
-  return{
-    price:state.car.price, 
-    additionalPrice:state.additionalPrice}
-}
+const mapStateToProps = (state) => {
+  return {
+    price: state.car.price,
+    additionalPrice: state.additionalPrice,
+  };
+};
 
-export default connect(mapStateToProps,{})(Total);
+export default connect(mapStateToProps, {})(Total);
